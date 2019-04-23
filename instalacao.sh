d61328fe65b57268bf601f5f9eb14595d05b1732
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-04-23 11:46:38 -0300

    Base criada!

---
+++ instalacao.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# AUTHORS: Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com> e Nicolas <nicolas.flores.contato@gmail.com>
+# DESCRIPTION: Instalações e configurações do linux que será criado.
+# VERSION: 0.0.1
+
+
+function main(){
+	echo "Hello, Cruel World!"
+}
+
+main()
