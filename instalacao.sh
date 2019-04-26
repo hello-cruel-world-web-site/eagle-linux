58be97e6029c67bba6db34855e0f10a0c9dd1f73
Nicolas Machado Flores <achronys@gmail.com>
2019-04-26 11:52:55 -0300

    Update instalacao.sh

--- instalacao.sh
+++ instalacao.sh
@@ -1,13 +1,54 @@
-#!/bin/bash
-
-#
-# AUTHORS: Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com> e Nicolas <nicolas.flores.contato@gmail.com>
-# DESCRIPTION: Instalações e configurações do linux que será criado.
-# VERSION: 0.0.1
 
 
-function main(){
-	echo "Hello, Cruel World!"
-}
-fdsfdssfdsffsdd
-main()
+
+sudo snap install code --classic
+sudo snap install zenity
+
+sudo snap install android-studio --classic
+sudo snap install go --classic
+
+sudo snap install ruby --classic
+sudo snap install bower --classic
+sudo snap install node --channel=10/stable --classic
+sudo snap install ubuntu-iso-download
+sudo snap install chromium
+sudo snap install firefox
+sudo snap install libreoffice
+sudo snap install opera
+sudo snap install mailspring
+sudo snap install mutt
+sudo snap install gnome-system-monitor
+sudo snap install snap-store
+sudo snap install whatsdesk
+sudo snap install zeronet
+
+sudo snap install vlc
+sudo snap install obs-studio
+
+sudo snap install docker
+sudo snap install postgresql10
+sudo snap install mysql
+
+sudo snap install spotify
+sudo snap install audacity
+
+sudo snap install gimp
+sudo snap install inkscape
+
+
+sudo flatpack install https://flathub.org/repo/appstream/org.gnome.Glade.flatpakref
+sudo flatpack install https://flathub.org/repo/appstream/com.valvesoftware.Steam.flatpakref
+sudo flatpack install https://flathub.org/repo/appstream/com.github.marktext.marktext.flatpakref
+sudo flatpack install https://flathub.org/repo/appstream/org.filezillaproject.Filezilla.flatpakref
+sudo flatpack install https://flathub.org/repo/appstream/com.slack.Slack.flatpakref
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 081525E2B4F1283B
+sudo apt-add-repository ppa:cubic-wizard/release
+sudo apt update
+sudo apt install cubic
+
+wget https://www.apachefriends.org/xampp-files/7.3.4/xampp-linux-x64-7.3.4-0-installer.run
+sudo chmod +x xampp-linux-x64-7.3.4-0-installer.run
+sudo bash xampp-linux-x64-7.3.4-0-installer.run
+
+sudo apt install zsh
